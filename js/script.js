e6ba1be52cd2cd0e3443169f4ecd51279ea8d4d6
Theint1997 <haymann.hh@gmail.com>
2020-11-23 19:18:46 +0630

    TheintHaymannHnin

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// window.onscroll=function(){myFun()}
+// var navbar=document.getElementById('navbar');
+
+// $(document).ready(function(){
+//     $(window).scroll(function() { 
+//         if ($(document).scrollTop() > 50) { 
+//             $(".navbar navbar-expand-lg navbar-light fixed-top").css("background-color", "#f8f8f8"); 
+//         } else {
+//         $(".navbar navbar-expand-lg navbar-light fixed-top").css("background-color", "transparent");
+//     }
+//     });
+// });
